c6b16e369b4ece47795fdd4378cf19a9686caaa1
Ashar Ardianto <ashardian.to21@gmail.com>
2021-10-02 09:54:33 +0700

    commit on 'main'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const menuToggle = document.querySelector('.nav button');
 const nav = document.querySelector('.nav-list');
+const translate = document.querySelectorAll(".translate");
 let heading = document.getElementById('heading');
 let thirdHeading = document.getElementById('third-heading');
 let subHeading = document.getElementById('sub-heading');
@@ -14,6 +15,14 @@ menuToggle.addEventListener('click', function() {
     nav.classList.toggle('nav-listdd');
 })
 
+window.addEventListener('scroll', function() {
+    let scroll = window.pageYOffset;
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        element.style.transform = `translateX(${scroll * speed}px)`
+    })
+})
+
 window.addEventListener('scroll', headingEffect);
 
 function headingEffect() {
@@ -23,16 +32,15 @@ function headingEffect() {
         thirdHeading.style.opacity = '0';       
         heading.style.transition = '1s ease-in-out';
         thirdHeading.style.transition = '1s ease-in-out';
-        heading.style.transform = 'translateX(-200px)'
-        thirdHeading.style.transform = 'translateX(200px)'
+        heading.style.transform = 'translateX(-200px)';
+        thirdHeading.style.transform = 'translateX(200px)';
     } else {
         heading.style.opacity = '1';
         thirdHeading.style.opacity = '1';
-        heading.style.transform = 'translateX(0px)'
-        thirdHeading.style.transform = 'translateX(0px)'
+        heading.style.transform = 'translateX(0px)';
+        thirdHeading.style.transform = 'translateX(0px)';
     }
-    // heading.style.marginRight = value * 5 + 'px';
-    // thirdHeading.style.marginLeft = value * 5 + 'px';      
+
 }
 
 
@@ -40,7 +48,7 @@ function headingEffect() {
 window.addEventListener('scroll', aboutEffect);
 
 function aboutEffect() {
-    if (window.scrollY >= 300) {
+    if (window.scrollY >= 800) {
         subHeading.style.opacity = '1';
         subHeading.style.transform = 'translateX(0px)';
         subHeading.style.transition = '0.5s ease-in-out';
